4933d4bb25a94ab02777f10dbf2963dc4afe5560
nafiz-hossain <nhremon8181@gmail.com>
2024-02-22 01:20:14 +0600

    Updated font color

--- src/utils/Themes.js
+++ src/utils/Themes.js
@@ -4,8 +4,8 @@ export const darkTheme = {
     bg: "#081B29", // Slightly Dark Gray
     bgLight: "#000000", // Dark Gray
     primary: "#0EA7F7", // Vivid Cyan
-    text_primary: "#D3D3D3", // Light Gray
-    text_secondary: "#e3e1e1", // Light Gray
+    text_primary: "#f7f7f7", // Light Gray
+    text_secondary: "#f0f0f0", // Light Gray
     card: "#001426", // Midnight Black
     card_light: "#031929", // Dark Slate Gray
     button: "#FFBB91", // Peach
@@ -17,8 +17,8 @@ export const lightTheme = {
     bg: "#081B29", // Slightly Dark Gray
     bgLight: "#000000", // Dark Gray
     primary: "#0EA7F7", // Vivid Cyan
-    text_primary: "#D3D3D3", // Light Gray
-    text_secondary: "#e3e1e1", // Light Gray
+    text_primary: "#f7f7f7", // Light Gray
+    text_secondary: "#f0f0f0", // Light Gray
     card: "#010f1c", // Midnight Black
     card_light: "#031929", // Dark Slate Gray
     button: "#FFBB91", // Peach
