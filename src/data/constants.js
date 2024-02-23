4af7ac02e62df78571fc8f6480777e52bd4fd34d
nafiz-hossain <nhremon8181@gmail.com>
2024-02-23 16:40:24 +0600

    Added static icons in assets to load website faster v2

--- src/data/constants.js
+++ src/data/constants.js
@@ -23,50 +23,50 @@ export const skills = [
       {
         name: "Postman",
         image:
-          "https://static-00.iconduck.com/assets.00/postman-icon-497x512-beb7sy75.png",
+          "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/postman.png",
       },
       {
         name: "JMeter",
-        image: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAAA/1BMVEVHcEy7m8TCss3gnKfLqsPqoZraanT99fP////VyNnqzNfUREr17OqziLfMstHSW23q1uW3ibS0s8iOiazOwdvbbXz0wJ/kl5/tsbLyqGzsl2zrtLzgmabzkirnj3r0plnib1H5t2ztlmPkhX32z8LexNrNdIzbnLC7RG3dwdjNhqHLGTCwFEnNOEvBFjjETGjMITbodCLaVEv2lhjgXjK5IknLbYm1eqewMWHJMEfLKj/DI0DrgC/TXG7jYyLWU173nCbZQST0qFz2tHbogFDeUiq7G0KiZpynT4eYWJXICirxlDvjdl/falrkbDnDl7mML3x6DXC9ZYmQRYnAXX+hsxQpAAAAK3RSTlMAYeacGUX9FQwJUP5bkMPPI/wtmGO4Ute0rP2NU6bSq/f0lKeRTNuSrnTn8GFIeAAAAOFJREFUKJF90sdywlAMBdBnAgZTTAiB9AQCKeQ127gNzUDoJZX//xYWDBu4Dy115kpaiJDDSlaH2aPm3j6bzSe19fsPCny2h8vODbb616hj22U8tOKOer3bFMTaYOWO3QS0ROSsB+NHvDFP9YzjXEO7s3hEKU/Da/SF3rKsKxi8b/MupRl8Tl50uWiVoMWl54u2OIP4xqScTGQS4jvzmefhYOov3DD2Cs1ozPww/DjHwcJPMAu+MRLzf/47fcGWLgTzaQ4byWoxzVAYITk1kbipNnKpncAi/pvd1NiJoHGBulvLmhlb+HG5vQAAAABJRU5ErkJggg=="
+        image: "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/jmeter.png"
     },
       {
         name: "Selenium",
-        image: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAAAPFBMVEUtrTkAoAAAowPc790AngAApxv///////8urjtJtVIZqint9+40rz6l1qhMtlRhvGiAx4XF5Me43rqPzZNSGtmfAAAACnRSTlNa////Wv//WlpaMxECygAAAPhJREFUKJF1ktmShSAMREkYcu+wyfL//zoddMSyIA+UeiqdTqP5detikY8xa0Z1D7nIHkbZQwpg9WcJqYFZMi/IrMcBFti9IBd8c14HeveGWdXIKuMVlJpwZJV/y+o0VFH2ho4G7eQekAnF/73pZBekjqVD8+ce9WIn1JBH5XvVCeGx5SNJ0BYiTzwhd6xFzD6iLeqSaRoa/muJ2oDbCMCN5kw7JtrMeKrQFYkPt8VehiDZO97STIiZsAG042Vb2g0peWaVtITk4qjbEP6XdGAVuLQSPFHP/ISjwnmVQe/tNsQ+aXxJp/sWEGRcBz8eHwlty5jvHn7/AJS7ESRok5XIAAAAAElFTkSuQmCC"
+        image: "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/selenium.png"
     },
 
     {
       name: "MQTTX",
-      image: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAMAAABEpIrGAAAASFBMVEVHcEwv0Y4hwn8y1JE+05Uz1ZIhw4Ahw4Atz4wBx32869X///8qy4gkx4Sq5MlWzpkfw38Zu3gEu3OB2rIavXkLsWwUt3QRtXE1qSAFAAAAGHRSTlMAT5Pf+v8Upf///////////s3//////08O1XRHAAABOElEQVR4AW2PBYKEMAwAQ6jhFsr/f3qRdqW3g6WdWQE+6LB33rseO/hBiN578XLzMbQefQPCJ6FvffJ9+PC+ZRhd8uHlU8swTj75VIuet3y5qp9k0ZvH1CB+0gnFhznNzCK3xId6DnQZOIiz4NZhNsxP9oHIgW2nyYrqNwkYgG5R5okLfhbv9sXoAGsgReMZhEOfOwdcnJf6a6h+PoDlvvAxWbHqcxtkS8wCohnarLhLce4FqMPZFPQOiI//BfGmCDiIuBBKcVlhe3QAUiHP6seTTi5qgPCQcRZ/83xzQcYDQJTlqF6XXPBDRgCIvJHzWXxWuFj0GTkIOpG9gY5aWBiAQR3NNyAo+ebD/F0Ou2UwgqzYM7WwW4BXcdNovl7V14KXLbl6A1uP0BDip44BfvDgIfLAB978ATSNLRZhl41BAAAAAElFTkSuQmCC"
+      image: "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/mqttx.png"
   },
     {
         name: "PyAutoGUI",
-        image: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgAgMAAAAOFJJnAAAACVBMVEVGUViMoa/i5+sa4fYGAAAAKklEQVR4AWPAAxhDQx1WrVrVgI0RwOqwgAEvgxbaEQy4GkwGhnbCDNwAAKEvTZMFFgusAAAAAElFTkSuQmCC"
+        image: "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/pyautogui.png"
     },
     {
         name: "Burp Suite",
-        image: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAAAOVBMVEVHcEz/ZjP/ZjP/YCj/pY///fz/ZjP/YCj/pY/////6ZTP/Vg3/y7//qpXsYjX/XCD/f1n/ZjP/5N4XOveAAAAAEnRSTlMAvd3b4Of/////6/////n///mvRk77AAAAnklEQVR4AZ3SBRLDMAwAwYYERtX//2tYQRVv0LMB0+N9Va01bde1zTasRqxBQ2ImBK3+hA40H5jjjm7EpINMzCw7Pg+IMhplMBFGCx5M9IW5qO24TYZFxMB5MnPRmJDwGt0Rw1wxEfKUJxv37fN5ysYS5tDCNTEw6noOn90TEd3EO6JuooXzJtpL0RO1cD/RHd1xtscTdf/fvveX+m0DMIATrHpZ6g8AAAAASUVORK5CYII="
+        image: "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/burpsuite.png"
     },
     {
         name: "GitHub Copilot",
-        image: "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png"
+        image: "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/GitHub-Mark.png"
     },
     {
         name: "ChatGPT",
-        image: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAAAXVBMVEV1qZxxp5lupZeqyMDC19Kbv7ZoopRtpZf////3+vn0+PeItKmXvLPi7Ol9rqJloZKBsKTb5+SUu7Hr8vDH2tVpo5Siw7u70sytysOyzcbV49++1M7p8O5enY3R4NyNarGBAAABK0lEQVR4Aa2SV2LEIAxEwUUMYCxjU9b1/sfMNtJ7Mr9PXSP+RbKq5IeobpqW3sWqBTRgLJGSr/M6uJ54MNDaj+olY+1YqsFhCsGgic9YrCdYWQXoxKQ44/RUmTwMqAcy8TiTpAVUmAoYa3CnJQsPmJEtrCwUK8+gcdtXNGw3eIv2DmVCX12g1n5BYk4AhjJpDXWFGCmOmCxTQCqZI6y6lmUhyAUcgheUVQmeZcDhrrBjZPWsaYtD8eDQEY1oCbmip00HADVzq73X5gznqkd6yhyWy3oN8u4ukFeIIouR7QQsgoU+w1OHoAqMl4m4HplWYCAYeCrssunKii7bh0pabPXzr1CGPsIE1yvJTrN86QO7GANtmS4byTcOutrgolaJ9xTHvCYlf2bOn+sB5MkR1bjopDwAAAAASUVORK5CYII="
+        image: "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/chatgpt.png"
     },
     {
         name: "Python Scripting",
-        image: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAAAh1BMVEVHcEwBVJc3caE2bJdKir3z2mL/6m05daZAfa5Jd5nsyUUqbKFHZ3flw0Towzv+2Uz842rmwTgANYDeuzlFgrL/6nD94WL20ETvxzf50T5Jh7lAfKxAf7E9eqv/5GX/4Fz/6HH/3VNFhbc1cKD/2kowapr/1Dj/10H+/v786a2dt851nL7c5OukoiX2AAAAHHRSTlMAf0dz/ovF/qRkVcoZX2noqUpnMo7r88Ww1sbqk9MnjgAAAR1JREFUKJFl0etygyAQBWCCEIJGozE1TWu9C8b2/Z+vsEscA+ef882RXSBky5nKKJL0TMJINrBhGOr6JwmYMrYCmQhfb8PfstYWhRhlgOvya3vCYBRgPWJPjAHSEwTsDeVpC9h8zfOc41RfjMES7p/jPF8/uq6r4DSwGocBQ2y4Qay5JcAsNk1zMZcW2iwNtW1rr2Yz4eyQg/UlkS9LDi4SrO/7AtGY5EeXCmr9NMWE4qDRscM0rjZNuiQEl0h3BLVJa7PKNxzI/ZrWd4ORHRTwVQNTqrBXlJgdLL79UqlP9yJSUg5WZbFNmmVZsXsZjqddbHTqvSffJmmVynx0p6nn8gwwdZMAFh6SOy6n1DbnW/eB9PDHcSnjotx//wNQ0jN2CWQnkAAAAABJRU5ErkJggg=="
+        image: "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/python-original.svg"
     },
 
     {
         name: "Bash Scripting",
-        image: "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAACAAAAAgCAAAAABWESUoAAABtklEQVR4AYXKEZDzCACG4c+htHauS5Wl6nqpWCndVEvRQCQQqoRCkZ1StVQpxy0zpVIsM8/dLvyd2b2de+WVJ/6ngKZp6qqY/enctMf+D+iabbLIoSkGoDtus8jmeEJo+2RbtF1ZtYCPtm2b/UvKMzH1SXUC0/0Bjwnm+iU10YxZ96C7dm35GLrT5QRzkUsj+kt2oK6TZJ8k7x0cU3XiXGYP5SZfTU2SlyMuWfefoMoOVZGvXtzWSbZn2uw70f/7s6pKVrf74H6sp1P9muKmzuEoykfyUb7myias79vza6PMa5PcCjFc8tmNU9sM5/Gs3FzJZx+jUH4JzKZl6puPhD7JUBMUwzEDpmncZEaWSDNUCDSHJfSj+X7DdX1n3NwgwAaWd4D9rr37e3yC4Yjd+MBtXfXGbXdqGk+wM9QvF7AqurEzjT1P0K9XeVx6DO7jWwXlEzA9LI0FdHdm7IcnAG+s4dGB95NvYMVhD3tg/R0UI7sO9uvdbpXrd+BAXwOo/ADFTBlYJYfrT6DGYlHwsOYn4MprgrH4bwB/ZTG/5/QrmFa3JDn/CsxFPqt/BcyLvC39Dn70D2T0GOzLn7S+AAAAAElFTkSuQmCC"
+        image: "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/shell.png"
     },
     {
       name: "Android Studio",
       image:
-        "https://developer.android.com/static/studio/images/new-studio-logo-1_1920.png",
+        "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/androidstudio.png",
     },
     ],
   }
@@ -76,31 +76,36 @@ export const skills = [
     skills: [
       {
         name: "Node Js",
-        image: "https://nodejs.org/static/images/logo.svg",
+        image: "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/node-js.svg",
       },
       {
         name: "Python",
         image:
-          "https://raw.githubusercontent.com/devicons/devicon/master/icons/python/python-original.svg",
+          "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/python-original.svg",
       },
       {
         name: "MySQL",
         image:
-          "https://raw.githubusercontent.com/devicons/devicon/master/icons/mysql/mysql-original-wordmark.svg",
+          "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/mysql-original-wordmark.svg",
       },
       {
         name: "Postgresql",
-        image: "https://www.postgresql.org/media/img/about/press/elephant.png",
+        image: "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/postgres.png",
+      },
+      {
+        name: "Redis",
+        image:
+          "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/redis.svg",
       },
       {
         name: "Java",
         image:
-          "https://raw.githubusercontent.com/devicons/devicon/master/icons/java/java-original.svg",
+          "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/java-original.svg",
       },
       {
         name: "Kotlin",
         image:
-          "https://www.vectorlogo.zone/logos/kotlinlang/kotlinlang-icon.svg",
+          "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/kotlinlang-icon.svg",
       },
 
     ],
@@ -111,13 +116,13 @@ export const skills = [
       {
         name: "Docker",
         image:
-          "https://raw.githubusercontent.com/devicons/devicon/master/icons/docker/docker-original-wordmark.svg",
+          "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/docker.png",
       },
 
       {
         name: "Jenkins",
         image:
-          "https://toppng.com/uploads/preview/jenkins-logo-11609365847mufysaivph.png",
+          "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/jenkins.png",
       },
     ],
   },
@@ -128,28 +133,28 @@ export const skills = [
       {
         name: "Git",
         image:
-          "https://e7.pngegg.com/pngimages/713/558/png-clipart-computer-icons-pro-git-github-logo-text-logo-thumbnail.png",
+          "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/git.jpg",
       },
       {
         name: "GitHub",
         image:
-          "https://github.githubassets.com/images/modules/logos_page/GitHub-Mark.png",
+          "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/GitHub-Mark.png",
       },
       {
         name: "VS Code",
         image:
-          "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9a/Visual_Studio_Code_1.35_icon.svg/512px-Visual_Studio_Code_1.35_icon.svg.png?20210804221519",
+          "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/vscode.png",
       },
 
       
       {
         name: "Figma",
         image:
-          "https://s3-alpha.figma.com/hub/file/1481185752/fa4cd070-6a79-4e1b-b079-8b9b76408595-cover.png",
+          "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/figma.png",
       },
       {
-        "name": "Asana",
-        "image": "data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAABwAAAAcCAMAAABF0y+mAAAAJ1BMVEVHcEzwamrxaWnwamrwaWnwaGjwamrwamrwaWnwaWnwaWnwaWnwamrh2b+jAAAADHRSTlMA9jDCGg7Y52hNoIUM/cV1AAAAsklEQVQokY2SWRLEIAhEBdzj/c87YlRIsFLDHz5pgda5PyMTAOUjCrGNiOEAJ+vUsqvtsMpRoCkNTcX71U/okrBkYBVYTUMeFgNvZymLFcucQ+rFQHhiLI1oJd+HKvcvuVu+DoyrzbnSvBrj23uENmpxpyAXexBDkvxSbgBPL7tgd/TCUauyLnxB9Qa7EZQ7tFc6Gni6U1R7t41SGlXzaW7MJzVYN6vvS3/mHPvnPhqn4weOiA9iH54cawAAAABJRU5ErkJggg=="
+        name: "Asana",
+        image: "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/asana.png"
       },
 
       
@@ -160,7 +165,7 @@ export const skills = [
 export const experiences = [
   {
     id: 0,
-    img: "https://www.tigerit.com/img/ti-logo.png",
+    img: "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/ti-logo.png",
     role: "Software Engineer QA",
     company: "Tiger IT Bangladesh Ltd",
     date: "June 2020 - Present",
@@ -179,7 +184,7 @@ export const experiences = [
 
   {
     id: 1,
-    img: "https://scontent.fdac138-2.fna.fbcdn.net/v/t39.30808-1/309164170_997868887727499_5322831563524362153_n.jpg?stp=c0.0.40.40a_cp0_dst-jpg_p40x40&_nc_cat=105&ccb=1-7&_nc_sid=596444&_nc_eui2=AeGDJvn5Fa_x6YC48BGLICBiAswz-oYQQdICzDP6hhBB0qB6eUO9wdRfKfTFrW6feRQZEEEfvt7f_eEyoFH-K7r5&_nc_ohc=fzVChql7xNcAX_x52TW&_nc_ht=scontent.fdac138-2.fna&oh=00_AfDTRTy7KOBwKZMmOauY7Lc8hJWwK5fjbjrIQ4uUejRGyQ&oe=65DB522D",
+    img: "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/project-feed-a-family.jpg",
     role: "Co Founder",
     company: "Project- Feed-a-Family",
     date: "Mar 2020 - Present",
@@ -194,7 +199,7 @@ export const experiences = [
 
   {
     id: 2,
-    img: "https://media.licdn.com/dms/image/C510BAQHpEnmIRD-HLA/company-logo_200_200/0/1630589374795?e=1716422400&v=beta&t=H0ZRqZMRRscKYMD2bO8bRS3wkXbiFH96z8EN3XSqhgc",
+    img: "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/csedusc.jpeg",
     role: "General Secretary",
     company: "CSEDU Students' Club",
     date: "Jan 2019 - Feb 2020",
@@ -212,7 +217,7 @@ export const experiences = [
 
   {
     id: 3,
-    img: "https://media.licdn.com/dms/image/C510BAQHpEnmIRD-HLA/company-logo_200_200/0/1630589374795?e=1716422400&v=beta&t=H0ZRqZMRRscKYMD2bO8bRS3wkXbiFH96z8EN3XSqhgc",
+    img: "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/csedusc.jpeg",
     role: "Executive Member",
     company: "CSEDU Students' Club",
     date: "Jan 2018 - Jan 2019",
@@ -230,7 +235,7 @@ export const experiences = [
 export const education = [
   {
     id: 0,
-    img: "https://upload.wikimedia.org/wikipedia/en/thumb/c/cb/Dhaka_University_logo.svg/1200px-Dhaka_University_logo.svg.png",
+    img: "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/Dhaka_University_logo.png",
     school: "University of Dhaka",
     date: "Jan 2015 - Dec 2019",
     publication: {
@@ -243,7 +248,7 @@ export const education = [
 
   {
     id: 1,
-    img: "https://jcc.edu.bd/wp-content/uploads/2022/11/cropped-CCJ-Logo_small.png",
+    img: "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/ccj.png",
     school: "Cantonment College, Jessore",
     date: "2011 - 2013",
     grade: "GPA 5.00",
@@ -253,7 +258,7 @@ export const education = [
 
   {
     id: 2,
-    img: "https://upload.wikimedia.org/wikipedia/bn/d/d4/%E0%A6%AF%E0%A6%B6%E0%A7%8B%E0%A6%B0%E0%A6%9C%E0%A6%BF%E0%A6%B2%E0%A6%BE%E0%A6%B8%E0%A7%8D%E0%A6%95%E0%A7%81%E0%A6%B2-%E0%A6%B2%E0%A7%8B%E0%A6%97%E0%A7%8B.png",
+    img: "https://raw.githubusercontent.com/nafiz-hossain/portfolio/main/src/images/icons/jzs.png",
     school: "Jessore Zilla School",
     date: "2005 - 2011",
     grade: "GPA 5.00",
